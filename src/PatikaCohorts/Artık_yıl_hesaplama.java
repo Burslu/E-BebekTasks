fd23fb20a8606710e28676bc95ad1f87934d7b8e
Burslu <iberkayuslu41@gmail.com>
2022-11-07 18:21:27 +0300

    Artik Yil Hesaplama

---
+++ src/PatikaCohorts/Artık_yıl_hesaplama.java
@@ -0,0 +1,20 @@
+package PatikaCohorts;
+
+import java.util.Scanner;
+
+public class Artık_yıl_hesaplama {
+    public static void main(String[] args) {
+        int year;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Year");//yil giriniz.
+        year = scanner.nextInt();
+
+        if ((year % 4 ==0) && (year %100 !=0) || (year % 400 == 0)){//if blogu icerisinde Genel bir kural olarak, artık yıllar 4 rakamının katı olan yıllardır,
+            // 100'ün katı olan yıllardan sadece 400'e kalansız olarak bölünebilenler artık .
+            System.out.println("Leap Year"+ year);
+        }else {
+            System.out.println("This is a not Leap Year"+ year);
+        }
+        scanner.close();
+    }
+}
