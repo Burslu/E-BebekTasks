8d93ad0c3761b08dc4604f90b49b29ea5594eacf
Burslu <iberkayuslu41@gmail.com>
2022-11-08 21:19:15 +0300

    Ebob Ekok

---
+++ src/PatikaCohorts/Ebob_Ekok.java
@@ -0,0 +1,35 @@
+package PatikaCohorts;
+
+import java.util.Scanner;
+
+public class Ebob_Ekok {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter smallNumber number:");
+        int smallNumber = scanner.nextInt();
+
+        System.out.println("Enter largesNumber number:");
+        int largeNumber = scanner.nextInt();
+        int t = 1;
+        int ebob =1;
+        int ekok =1;
+
+        while (t<=smallNumber && t<=largeNumber){
+            if (smallNumber % t == 0 && largeNumber % t == 0){
+                ebob = t;
+            }
+            t++;
+        }
+        System.out.println("Ebob :" + ebob);
+
+        while (t<=(largeNumber*smallNumber)){
+            if (t % smallNumber == 0 && t % largeNumber ==0){
+                ekok = t;
+            }
+        }
+        System.out.println("Ekok"+ekok);
+
+
+    }
+}
